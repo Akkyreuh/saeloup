b75bf807c8976fedfbd5807a894b2ebb5732c1c6
louis <louisbenel12@gmail.com>
2023-12-12 14:25:26 +0100

    test commit

--- app/src/main/java/com/example/saeloup/MainActivity.kt
+++ app/src/main/java/com/example/saeloup/MainActivity.kt
@@ -3,21 +3,17 @@ package com.example.saeloup
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
-import androidx.compose.foundation.layout.Arrangement
-import androidx.compose.foundation.layout.Column
-import androidx.compose.foundation.layout.fillMaxSize
-import androidx.compose.foundation.layout.padding
-import androidx.compose.material3.Button
-import androidx.compose.material3.ExperimentalMaterial3Api
-import androidx.compose.material3.MaterialTheme
-import androidx.compose.material3.Surface
-import androidx.compose.material3.Text
-import androidx.compose.material3.TextField
+import androidx.compose.foundation.layout.*
+import androidx.compose.material3.*
 import androidx.compose.runtime.*
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
-import androidx.compose.ui.res.stringResource
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
+import androidx.navigation.NavController
+import androidx.navigation.compose.NavHost
+import androidx.navigation.compose.composable
+import androidx.navigation.compose.rememberNavController
 import com.example.saeloup.ui.theme.SaeloupTheme
 
 class MainActivity : ComponentActivity() {
@@ -25,19 +21,15 @@ class MainActivity : ComponentActivity() {
         super.onCreate(savedInstanceState)
         setContent {
             SaeloupTheme {
-                // A surface container using the 'background' color from the theme
-                Surface(modifier = Modifier.fillMaxSize(), color = MaterialTheme.colorScheme.background) {
-                    Column(
-                        modifier = Modifier
-                            .fillMaxSize()
-                            .padding(16.dp),
-                        verticalArrangement = Arrangement.Top,
-                        horizontalAlignment = androidx.compose.ui.Alignment.CenterHorizontally
-                    ) {
-                        Greeting("ROARRR", modifier = Modifier.padding(top = 32.dp))
-                        SimpleFilledTextFieldSample("PSEUDO :")
-                        SimpleFilledTextFieldSample("CODE PIN")
-                        ValiderButton()
+                // Setup NavController
+                val navController = rememberNavController()
+
+                NavHost(navController = navController, startDestination = "main") {
+                    composable("main") {
+                        MainScreen(navController)
+                    }
+                    composable("newScreen") {
+                        NewScreen()
                     }
                 }
             }
@@ -46,13 +38,40 @@ class MainActivity : ComponentActivity() {
 }
 
 @Composable
+fun MainScreen(navController: NavController) {
+    Surface(
+        modifier = Modifier.fillMaxSize(),
+        color = MaterialTheme.colorScheme.background
+    ) {
+        Column(
+            modifier = Modifier
+                .fillMaxSize()
+                .padding(16.dp)
+                .padding(top = 64.dp),
+            verticalArrangement = Arrangement.Center,
+            horizontalAlignment = Alignment.CenterHorizontally
+        ) {
+            Greeting("ROARRR")
+            Spacer(modifier = Modifier.height(16.dp))
+            SimpleFilledTextFieldSample("PSEUDO :")
+            SimpleFilledTextFieldSample("CODE PIN")
+            Spacer(modifier = Modifier.weight(1f))
+            ValiderButton(onClick = { navController.navigate("newScreen") })
+        }
+    }
+}
+
+@Composable
+fun NewScreen() {
+    Text("Nouvelle Page")
+}
+
+@Composable
 fun Greeting(name: String, modifier: Modifier = Modifier) {
     Text(
         text = name,
         modifier = modifier,
-        style = MaterialTheme.typography.h4.copy(fontSize = 30.sp)
     )
-}
 }
 
 @OptIn(ExperimentalMaterial3Api::class)
@@ -67,13 +86,12 @@ fun SimpleFilledTextFieldSample(label: String) {
     )
 }
 
-@OptIn(ExperimentalMaterial3Api::class)
 @Composable
-fun ValiderButton() {
+fun ValiderButton(onClick: () -> Unit) {
     Button(
-        onClick = { /* Traitement à effectuer lors du clic sur le bouton Valider */ },
+        onClick = onClick,
         content = { Text(text = "Valider") },
-        modifier = Modifier.padding(top = 16.dp)
+        modifier = Modifier.padding(bottom = 16.dp)
     )
 }
 
