36c39db44898ef8a23324fc1e7b3928cb708bba3
louis <louisbenel12@gmail.com>
2023-12-12 14:46:55 +0100

    commit

--- app/src/main/java/com/example/saeloup/MainActivity.kt
+++ app/src/main/java/com/example/saeloup/MainActivity.kt
@@ -1,15 +1,24 @@
 package com.example.saeloup
-
+import androidx.compose.foundation.BorderStroke
+import androidx.compose.foundation.Image
+import androidx.compose.foundation.background
+import androidx.compose.foundation.layout.*
+import androidx.compose.foundation.shape.CircleShape
+import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.filled.Menu
+import androidx.compose.material.icons.filled.MoreVert
+import androidx.compose.material3.*
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.res.painterResource
+import androidx.compose.ui.unit.dp
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
-import androidx.compose.foundation.layout.*
-import androidx.compose.material3.*
 import androidx.compose.runtime.*
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.tooling.preview.Preview
-import androidx.compose.ui.unit.dp
 import androidx.navigation.NavController
 import androidx.navigation.compose.NavHost
 import androidx.navigation.compose.composable
@@ -31,11 +40,18 @@ class MainActivity : ComponentActivity() {
                     composable("newScreen") {
                         NewScreen()
                     }
+                    composable("Card") {
+                        NewScreen()
+                    }
                 }
             }
         }
     }
 }
+
+
+
+
 
 @Composable
 fun MainScreen(navController: NavController) {
@@ -61,11 +77,135 @@ fun MainScreen(navController: NavController) {
     }
 }
 
+
+@OptIn(ExperimentalMaterial3Api::class)
 @Composable
 fun NewScreen() {
-    Text("Nouvelle Page")
+    Scaffold(
+        topBar = {
+            SmallTopAppBar(
+                title = {},
+                navigationIcon = {
+                    // Placeholder for the button at the top left
+                    IconButton(onClick = { /* TODO: Handle left icon click */ }) {
+                        Icon(Icons.Filled.Menu, contentDescription = "Menu")
+                    }
+                },
+                actions = {
+                    // Placeholder for the button at the top right
+                    IconButton(onClick = { /* TODO: Handle right icon click */ }) {
+                        Icon(Icons.Filled.MoreVert, contentDescription = "More actions")
+                    }
+                }
+            )
+        }
+    ) { innerPadding ->
+        Column(
+            modifier = Modifier
+                .padding(innerPadding)
+                .fillMaxSize(),
+            verticalArrangement = Arrangement.Center,
+            horizontalAlignment = Alignment.CenterHorizontally
+        ) {
+            // Circle for the image at the center of the screen
+            Box(
+                contentAlignment = Alignment.Center,
+                modifier = Modifier
+                    .size(250.dp)
+                    .background(Color.Green.copy(alpha = 0.2f), CircleShape)
+                    .padding(16.dp)
+            ) {
+                // Placeholder for the image
+                // Replace with an actual Image composable when you have the image resource
+                Image(
+                    painter = painterResource(id = R.drawable.fourche),
+                    contentDescription = "Center Image",
+                    modifier = Modifier.size(200.dp).padding(8.dp)
+                )
+            }
+            Spacer(modifier = Modifier.height(24.dp))
+            // Your form or any other composables go here
+            // For example, a dropdown menu or a list of players
+            DropdownMenuSample()
+            Spacer(modifier = Modifier.height(24.dp))
+            // Send button
+            Button(
+                onClick = { /* TODO: Handle send button click */ },
+                modifier = Modifier.padding(16.dp)
+            ) {
+                Text("Vote")
+            }
+        }
+    }
 }
 
+@OptIn(ExperimentalMaterial3Api::class)
+@Composable
+fun DropdownMenuSample() {
+    var expanded by remember { mutableStateOf(false) }
+    val items = List(10) { "Joueur $it" }
+    var selectedIndex by remember { mutableStateOf(0) }
+
+    ExposedDropdownMenuBox(
+        expanded = expanded,
+        onExpandedChange = {
+            expanded = !expanded
+        }
+    ) {
+        TextField(
+            readOnly = true,
+            value = items[selectedIndex],
+            onValueChange = { },
+            label = { Text("Sélectionnez un joueur") },
+            trailingIcon = {
+                ExposedDropdownMenuDefaults.TrailingIcon(expanded = expanded)
+            },
+            colors = ExposedDropdownMenuDefaults.textFieldColors()
+        )
+        ExposedDropdownMenu(
+            expanded = expanded,
+            onDismissRequest = {
+                expanded = false
+            }
+        ) {
+            items.forEachIndexed { index, selectionOption ->
+                DropdownMenuItem(
+                    text = { Text(selectionOption) },
+                    onClick = {
+                        selectedIndex = index
+                        expanded = false
+                    }
+                )
+            }
+        }
+    }
+}
+
+
+/**
+@Composable
+fun NewScreen() {
+    Column(
+        modifier = Modifier
+            .fillMaxSize()
+            .padding(16.dp),
+        verticalArrangement = Arrangement.Center,
+        horizontalAlignment = Alignment.CenterHorizontally
+    ) {
+        Greeting("ROARRR")
+        Spacer(modifier = Modifier.height(16.dp))
+
+        // Afficher les joueurs de 1 à 10
+        for (i in 1..10) {
+            Text("Joueur $i")
+            Spacer(modifier = Modifier.height(8.dp))
+        }
+
+        Spacer(modifier = Modifier.weight(1f)) // Utiliser l'espace restant
+        ValiderButton(onClick = { /* Action pour le bouton Valider */ })
+    }
+}
+**/
 @Composable
 fun Greeting(name: String, modifier: Modifier = Modifier) {
     Text(
