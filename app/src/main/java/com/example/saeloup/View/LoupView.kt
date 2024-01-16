ed4cf734546faa2162ff4c5d793f27bae417a2c9
Keran Saint-Blancat <keran.blancat@mediatropy.com>
2024-01-16 16:23:25 +0100

    loupss

--- app/src/main/java/com/example/saeloup/View/LoupView.kt
+++ app/src/main/java/com/example/saeloup/View/LoupView.kt
@@ -1,6 +1,7 @@
 package com.example.saeloup.View
 
 import android.util.Log
+import android.widget.Toast
 import androidx.compose.foundation.Image
 import androidx.compose.foundation.background
 import androidx.compose.foundation.layout.Arrangement
@@ -8,6 +9,7 @@ import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.layout.size
@@ -20,11 +22,14 @@ import androidx.compose.material3.Button
 import androidx.compose.material3.DropdownMenu
 import androidx.compose.material3.DropdownMenuItem
 import androidx.compose.material3.ExperimentalMaterial3Api
+import androidx.compose.material3.ExposedDropdownMenuBox
+import androidx.compose.material3.ExposedDropdownMenuDefaults
 import androidx.compose.material3.Icon
 import androidx.compose.material3.IconButton
 import androidx.compose.material3.Scaffold
 import androidx.compose.material3.SmallTopAppBar
 import androidx.compose.material3.Text
+import androidx.compose.material3.TextField
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.LaunchedEffect
 import androidx.compose.runtime.getValue
@@ -34,6 +39,8 @@ import androidx.compose.runtime.setValue
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.layout.ContentScale
+import androidx.compose.ui.platform.LocalContext
 import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.unit.dp
 import androidx.navigation.NavController
@@ -48,6 +55,7 @@ import com.google.firebase.database.database
 
 
 
+
 class LoupView {
 }
 
@@ -56,7 +64,12 @@ class LoupView {
 fun Loup(navController: NavController) {
     val deroulement = remember { mutableStateOf("") }
     val shouldNavigate = remember { mutableStateOf(false) }
+
+    val context = LocalContext.current
+    val coffeeDrinks = arrayOf("Americano", "Cappuccino", "Espresso", "Latte", "Mocha")
     var expanded by remember { mutableStateOf(false) }
+    var selectedText by remember { mutableStateOf(coffeeDrinks[0]) }
+
 
     val partiePath = AppState.currentJoueurPath?.substringBefore("/Joueurs") ?: ""
 
@@ -103,65 +116,84 @@ fun Loup(navController: NavController) {
             )
         }
     ) { innerPadding ->
-        Column(
-            modifier = Modifier
-                .padding(innerPadding)
-                .fillMaxSize(),
-            verticalArrangement = Arrangement.Center,
-            horizontalAlignment = Alignment.CenterHorizontally
-        ) {
-            // Circle for the image at the center of the screen
-            Box(
-                contentAlignment = Alignment.Center,
+
+            Column(
                 modifier = Modifier
-                    .size(250.dp)
-                    .background(Color(0xFFF46363), CircleShape)
-                    .padding(16.dp)
+                    .padding(innerPadding)
+                    .fillMaxSize(),
+                verticalArrangement = Arrangement.Center,
+                horizontalAlignment = Alignment.CenterHorizontally
             ) {
-                // Placeholder for the image
-                // Replace with an actual Image composable when you have the image resource
-                Image(
-                    painter = painterResource(id = R.drawable.loup),
-                    contentDescription = "Center Image",
+                // Circle for the image at the center of the screen
+                Box(
+                    contentAlignment = Alignment.Center,
                     modifier = Modifier
-                        .size(168.dp)
-                        .padding(8.dp)
-                )
-            }
-            Spacer(modifier = Modifier.height(90.dp))
-            // Your form or any other composables go here
-            // For example, a dropdown menu or a list of players
-
-            Box(modifier = Modifier.fillMaxSize(), contentAlignment = Alignment.TopStart) {
-                IconButton(onClick = { expanded = true }) {
-                    Icon(Icons.Filled.MoreVert, contentDescription = "Menu")
-                }
-
-                DropdownMenu(
-                    expanded = expanded,
-                    onDismissRequest = { expanded = false },
+                        .size(250.dp)
+                        .background(Color(0xFFF46363), CircleShape)
+                        .padding(16.dp)
                 ) {
-                    DropdownMenuItem(onClick = {
-                        // Votre code ici. Ce bloc ne retourne rien.
-                    }) {
-                        Text("Option")
+                    // Placeholder for the image
+                    // Replace with an actual Image composable when you have the image resource
+                    Image(
+                        painter = painterResource(id = R.drawable.loup),
+                        contentDescription = "Center Image",
+                        modifier = Modifier
+                            .size(168.dp)
+                            .padding(8.dp)
+                    )
+                }
+                Spacer(modifier = Modifier.height(90.dp))
+                // Your form or any other composables go here
+                // For example, a dropdown menu or a list of players
+
+
+                Box(
+                    modifier = Modifier
+                        .fillMaxWidth()
+                        .padding(32.dp),
+                    contentAlignment = Alignment.Center
+                ) {
+                    ExposedDropdownMenuBox(
+                        expanded = expanded,
+                        onExpandedChange = {
+                            expanded = !expanded
+                        }
+                    ) {
+                        TextField(
+                            value = selectedText,
+                            onValueChange = {},
+                            readOnly = true,
+                            trailingIcon = { ExposedDropdownMenuDefaults.TrailingIcon(expanded = expanded) },
+                            modifier = Modifier.menuAnchor()
+                        )
+
+                        ExposedDropdownMenu(
+                            expanded = expanded,
+                            onDismissRequest = { expanded = false }
+                        ) {
+                            coffeeDrinks.forEach { item ->
+                                DropdownMenuItem(
+                                    text = { Text(text = item) },
+                                    onClick = {
+                                        selectedText = item
+                                        expanded = false
+                                        Toast.makeText(context, item, Toast.LENGTH_SHORT).show()
+                                    }
+                                )
+                            }
+                        }
                     }
+                }
 
-
-
-                    // Ajoutez plus d'options ici
+                Spacer(modifier = Modifier.height(24.dp))
+                // Send button
+                Button(
+                    onClick = {},
+                    modifier = Modifier.padding(16.dp)
+                ) {
+                    Text("Vote")
                 }
             }
 
-
-            Spacer(modifier = Modifier.height(24.dp))
-            // Send button
-            Button(
-                onClick = {},
-                modifier = Modifier.padding(16.dp)
-            ) {
-                Text("Vote")
-            }
-        }
     }
 }
